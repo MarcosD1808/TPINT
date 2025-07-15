fb3a3c3a10951e3f5b12f6f9a522d50df962c2a1
FraNiPe <franperez@alumno.unlam.edu.ar>
2025-07-15 15:57:04 -0300

    fav perfil

--- JavaScript/perfil.js
+++ JavaScript/perfil.js
@@ -6,28 +6,19 @@ const series = seriesJSON ? JSON.parse(seriesJSON) : DATA_SERIES;
 let usuario = JSON.parse(localStorage.getItem("usuarioActual"));
 let contenedorP=document.getElementById("ContainerPelis")
 let contenedorS=document.getElementById("ContainerSeries")
-const favoritos = usuario?.favoritos
-
-function filtrarFavoritos(favoritosStrings, contenidoArray) {
-  return contenidoArray.filter(item =>
-    favoritosStrings.some(fav => fav.trim().toLowerCase() === item.titulo.trim().toLowerCase())
-  );
-}
-const peliculasFavoritas = filtrarFavoritos(favoritos, peliculas);
-const seriesFavoritas = filtrarFavoritos(favoritos, series);
-console.log("Películas favoritas:", peliculasFavoritas);
-console.log("Series favoritas:", seriesFavoritas);
+const peliculasFavoritas = usuario.pelisfavoritos
+const seriesFavoritas = usuario.seriesfavoritos
 
     peliculasFavoritas.forEach((peli) => {
     const card = document.createElement("div");
     card.className = "CardsContenidos";
     card.innerHTML = `  
       <ul class="CardTitle">
-      <li><a class="IDTitle" href="DetallePeli.html?id=${peliculas.indexOf(peli)}">${peli.titulo}</a></li>
-      <li><span class="material-icons fav" >favorite</span></li>
+      <li><a class="IDTitle" href="DetallePeli.html?id=${peli}">${peliculas[peli].titulo}</a></li>
+      <li><span class="material-icons fav" data-tipo="Peli" data-id=${peli}>favorite</span></li>
       </ul> 
-      <a href="DetallePeli.html?id=${peliculas.indexOf(peli)}">     
-        <img class="PosterContenidos" src="${peli.portadaJPG}" alt="${peli.titulo}">
+      <a href="DetallePeli.html?id=${peli}">     
+        <img class="PosterContenidos" src="${peliculas[peli].portadaJPG}" alt="${peliculas[peli].titulo}">
       </a> `;
     contenedorP.appendChild(card);
   });
@@ -36,11 +27,11 @@ seriesFavoritas.forEach((serie) => {
     card.className = "CardsContenidos";
     card.innerHTML = `
       <ul class="CardTitle">
-      <li><a class="IDTitle" href="detalle-series.html?id=${series.indexOf(serie)}">${serie.titulo}</a></li>
-      <li><span class="material-icons fav" >favorite</span></li>
+      <li><a class="IDTitle" href="detalle-series.html?id=${serie}">${series[serie].titulo}</a></li>
+      <li><span class="material-icons fav" data-tipo="serie" data-id=${serie} >favorite</span></li>
       </ul>  
-      <a href="detalle-series.html?id=${series.indexOf(serie)}">
-        <img class="PosterContenidos" src="${serie.portadaJPG}" alt="${serie.titulo}">
+      <a href="detalle-series.html?id=${serie}">
+        <img class="PosterContenidos" src="${series[serie].portadaJPG}" alt="${series[serie].titulo}">
       </a>`;
     contenedorS.appendChild(card);  });
 
